c35f53adca322eff9559ab17167cc8b772afbd59
Salabson <salabson4@yahoo.co.uk>
2016-02-11 16:48:44 +0000

    Feat:set motor power with gopigo driver

--- src/gopigo_interface/src/gopigo3_interface.py
+++ src/gopigo_interface/src/gopigo3_interface.py
@@ -2,10 +2,12 @@
 
 import rospy
 from std_msgs.msg import Float32
+import gopigo3
 
 rwheel_motorcmd = Float32()
 lwheel_motorcmd = Float32()
 
+GPG = gopigo3.GoPiGo3()
 
 def rwheel_motorcmd_callback(msg):
     rwheel_motorcmd.data = msg.data
@@ -24,7 +26,13 @@ if __name__ == '__main__' :
     
     # Create subscriber objects
     rwheel_motorcmd_sub = rospy.Subscriber("/rwheel_motorcmd", Float32, rwheel_motorcmd_callback)
-    lwheel_motorcmd_sub = rospy.Subscriber("/rwheel_motorcmd", Float32, lwheel_motorcmd_callback)
+    lwheel_motorcmd_sub = rospy.Subscriber("/lwheel_motorcmd", Float32, lwheel_motorcmd_callback)
+    
+    rate = rospy.Rate(20)
+    while not rospy.is_shutdown():
+	GPG.set_motor_power(GPG.MOTOR_RIGHT,rwheel_motorcmd.data)
+	GPG.set_motor_power(GPG.MOTOR_LEFT,lwheel_motorcmd.data)
+	rate.sleep()
     
     # call the callbacks
     rospy.spin()
