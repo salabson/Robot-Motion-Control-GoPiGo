a0f5dbb8b1deb722edc1be989e33479661af4e2d
salabson <salabson4@yahoo.co.uk>
2019-10-14 10:58:15 +0100

    Fix:calculate rigth and left wheels tangential velocities

--- src/diffdrive_controller/src/diffdrive_controller.cpp
+++ src/diffdrive_controller/src/diffdrive_controller.cpp
@@ -2,10 +2,29 @@
 #include<std_msgs/Float32.h>
 #include<geometry_msgs/Twist.h>
 
+double robot_width;
+
 void cmd_vel_callback(const geometry_msgs::Twist::ConstPtr& twist_msg){
         double vel_x = twist_msg->linear.x;
         double vel_th = twist_msg->angular.z;
-        ROS_INFO("vel_x: %f angular_z: %f", vel_x, vel_th);
+        
+        // right and left wheel tangential velocities
+        double vr = 0.0;
+        double vl = 0.0;
+
+        if(vel_x == 0.0){
+                // robot turning in place
+                vr = vel_th*robot_width/2.0;
+                vl = -1*vr;
+        } else if(vel_th == 0.0){
+                // robot moving forward/backward
+                vr = vl = vel_x;
+        }else {
+                // robot moving in arc: both vel_x and vel_th are zero
+                vr = vel_x + vel_th*robot_width/2.0;
+                vl = vel_x - vel_th*robot_width/2.0;
+
+        }
 }
 int main(int argc, char** argv){
         // Initialize ROS system and create node object
