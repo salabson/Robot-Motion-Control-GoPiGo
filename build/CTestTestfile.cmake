6a7b36aeb379b4b3ee69aa5971aa11d23d49ed05
salabson <salabson4@yahoo.co.uk>
2019-10-14 14:11:30 +0100

    Chore:update cmakelists file

--- build/CTestTestfile.cmake
+++ build/CTestTestfile.cmake
@@ -5,4 +5,5 @@
 # This file includes the relevant testing commands required for 
 # testing this directory and lists subdirectories to be tested as well.
 subdirs(gtest)
+subdirs(base_controller)
 subdirs(diffdrive_controller)
