c7a3bf3d017e2ced4448330682eb09fc44e0bd9c
salabson <salabson4@yahoo.co.uk>
2019-10-14 20:43:10 +0100

    Feat:calculate motor command from angular velocity

--- src/base_controller/src/base_controller.cpp
+++ src/base_controller/src/base_controller.cpp
@@ -1,15 +1,66 @@
 #include<ros/ros.h>
 #include<std_msgs/Float32.h>
+#include<math.h>
 
 double rwheel_tangential_vel;
 double lwheel_tangential_vel;
 
 double wheel_radius;
 
+double max_ang_vel = 5.0;
+double min_ang_vel = 1.90;
+double max_motor_cmd = 100;
+double min_motor_cmd = 43;
+
+
 double tangential_2_angular_vel(double tang_vel){
         return tang_vel/wheel_radius;
 
 }
+
+double angular_2_motor_cmd(double angular_vel){
+        double slope = (max_motor_cmd-min_motor_cmd)/(max_ang_vel-min_ang_vel);
+        double intercept = max_motor_cmd - slope*angular_vel;
+        double motor_cmd;
+       // positive angular velocity
+       if(angular_vel >= 0){
+               motor_cmd = slope*angular_vel+intercept;
+               
+               // clip motor command to maximum motor command
+               if(motor_cmd > max_motor_cmd){
+                       motor_cmd = max_motor_cmd;
+               }
+               
+               // clip motor command to minimum motor command
+               if(max_motor_cmd < min_motor_cmd){
+                       motor_cmd = min_motor_cmd;
+               }
+
+               return motor_cmd;
+        
+       }
+
+       // negative angular velocity
+        if(angular_vel < 0){
+               motor_cmd = slope*abs(angular_vel)+intercept;
+               
+               // clip motor command to maximum motor command
+               if(motor_cmd > max_motor_cmd){
+                       motor_cmd = max_motor_cmd;
+               }
+               
+               // clip motor command to minimum motor command
+               if(max_motor_cmd < min_motor_cmd){
+                       motor_cmd = min_motor_cmd;
+               }
+               
+               return -motor_cmd;
+       }
+
+
+}
+
+
 
 void rwheel_tang_vel_callback(const std_msgs::Float32::ConstPtr& rtang_vel){
         rwheel_tangential_vel = rtang_vel->data;
