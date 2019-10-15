38b1fe87d586da38a38be7ea53263f1cd8992ddb
salabson <salabson4@yahoo.co.uk>
2019-10-15 10:55:13 +0100

    Fix:fix error of motor commands producing nan result by intializing wheel radius

--- src/base_controller/src/base_controller.cpp
+++ src/base_controller/src/base_controller.cpp
@@ -8,7 +8,7 @@ std_msgs::Float32 lwheel_motorcmd;
 double rwheel_tangential_vel;
 double lwheel_tangential_vel;
 
-double wheel_radius;
+double wheel_radius = 0.03;
 
 double max_ang_vel = 5.0;
 double min_ang_vel = 1.90;
@@ -16,8 +16,23 @@ double max_motor_cmd = 100;
 double min_motor_cmd = 43;
 
 
+
+
+void rwheel_tang_vel_callback(const std_msgs::Float32::ConstPtr& rtang_vel){
+        rwheel_tangential_vel = rtang_vel->data;
+	ROS_INFO("rtan: %f",rwheel_tangential_vel );
+    
+}
+
+void lwheel_tang_vel_callback(const std_msgs::Float32::ConstPtr& ltang_vel){
+        lwheel_tangential_vel= ltang_vel->data;
+	ROS_INFO("ltan: %f",lwheel_tangential_vel );
+
+}
+
 double tangential_2_angular_vel(double tang_vel){
-        return tang_vel/wheel_radius;
+       double  result = tang_vel/wheel_radius;
+        ROS_INFO("Ang vel : %f", result);
 
 }
 
@@ -38,7 +53,8 @@ double angular_2_motor_cmd(double angular_vel){
                if(max_motor_cmd < min_motor_cmd){
                        motor_cmd = min_motor_cmd;
                }
-
+               
+               ROS_INFO("motor_cmd p: %f",motor_cmd);
                return motor_cmd;
         
        }
@@ -56,30 +72,20 @@ double angular_2_motor_cmd(double angular_vel){
                if(max_motor_cmd < min_motor_cmd){
                        motor_cmd = min_motor_cmd;
                }
-               
+
                return -motor_cmd;
        }
 
 
 }
 
+
+
+
 double  calculate_motorcmd(double tang_vel){
         double angular_vel = tangential_2_angular_vel(tang_vel);
         return angular_2_motor_cmd(angular_vel);
         
-}
-
-
-void rwheel_tang_vel_callback(const std_msgs::Float32::ConstPtr& rtang_vel){
-        rwheel_tangential_vel = rtang_vel->data;
-	ROS_INFO("rtan: %f",rwheel_tangential_vel );
-    
-}
-
-void lwheel_tang_vel_callback(const std_msgs::Float32::ConstPtr& ltang_vel){
-        lwheel_tangential_vel= ltang_vel->data;
-	ROS_INFO("ltan: %f",lwheel_tangential_vel );
-
 }
 
 int main(int argc, char** argv){
@@ -99,6 +105,7 @@ int main(int argc, char** argv){
 
         //start publishing
         while(ros::ok()){ 
+                ROS_INFO("rtan: %f ltan: %f", rwheel_tangential_vel, lwheel_tangential_vel);
                 rwheel_motorcmd.data = calculate_motorcmd(rwheel_tangential_vel);
                 lwheel_motorcmd.data = calculate_motorcmd(lwheel_tangential_vel);
 
