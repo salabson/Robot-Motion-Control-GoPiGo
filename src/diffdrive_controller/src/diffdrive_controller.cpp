451ee71166711ca867f078e78e66b12acc24137a
salabson <salabson4@yahoo.co.uk>
2019-10-17 14:05:39 +0100

    Refactor:refactor diffdrive code to class

--- src/diffdrive_controller/src/diffdrive_controller.cpp
+++ src/diffdrive_controller/src/diffdrive_controller.cpp
@@ -2,57 +2,95 @@
 #include<std_msgs/Float32.h>
 #include<geometry_msgs/Twist.h>
 
-std_msgs::Float32 rwheel_tangential_vel;
-std_msgs::Float32 lwheel_tangential_vel ;
-double robot_width = 0.14;
 
-void cmd_vel_callback(const geometry_msgs::Twist::ConstPtr& twist_msg){
-        double vel_x = twist_msg->linear.x;
-        double vel_th = twist_msg->angular.z;
+
+class DiffDrive{
+	private:
+		std_msgs::Float32 rwheel_tangential_vel;
+		std_msgs::Float32 lwheel_tangential_vel;
+		
+		//Distance between the wheels
+		double robot_width;
+
+	        // Create subscriber object
+       		ros::Subscriber vel_sub;
         
-        // right and left wheel tangential velocities
-        double vr = 0.0;
-        double vl = 0.0;
+        	// Create publisher objects
+        	ros::Publisher  rwheel_tang_vel_pub; 
+        	ros::Publisher  lwheel_tang_vel_pub; 
+		
+	public:
+		//Create class constructor
+		DriffDrive(ros::NodeHandle nh*){
+			robot_width=0.14;
+			
+			//Initialize subscriber object
+        		vel_sub = nh->subscribe("/cmd_vel", 10, &DiffDrive::cmd_vel_callback, this);
+        
+        		//Initialize publisher objects
+       			rwheel_tang_vel_pub = nh->advertise<std_msgs::Float32>("/rwheel_tangential_vel", 10); 
+        		lwheel_tang_vel_pub = nh->advertise<std_msgs::Float32>("/lwheel_tangential_vel", 10); 
+		}
+        
+		
+		// Velocity callback
+		void cmd_vel_callback(const geometry_msgs::Twist::ConstPtr& twist_msg){
+			double vel_x = twist_msg->linear.x;
+			double vel_th = twist_msg->angular.z;
+		
+			// right and left wheel tangential velocities
+			double vr = 0.0;
+			double vl = 0.0;
 
-        if(vel_x == 0.0){
-                // robot turning in place
-                vr = vel_th*robot_width/2.0;
-                vl = -1*vr;
-        } else if(vel_th == 0.0){
-                // robot moving forward/backward
-                vr = vl = vel_x;
-        }else {
-                // robot moving in arc: both vel_x and vel_th are zero
-                vr = vel_x + vel_th*robot_width/2.0;
-                vl = vel_x - vel_th*robot_width/2.0;
+			if(vel_x == 0.0){
+				// robot turning in place
+				vr = vel_th*robot_width/2.0;
+				vl = -1*vr;
+			} else if(vel_th == 0.0){
+				// robot moving forward/backward
+				vr = vl = vel_x;
+			}else {
+				// robot moving in arc: both vel_x and vel_th are zero
+				vr = vel_x + vel_th*robot_width/2.0;
+				vl = vel_x - vel_th*robot_width/2.0;
 
-        }
+			}
 
-        rwheel_tangential_vel.data = vr;
-        lwheel_tangential_vel.data = vl;
-}
+			rwheel_tangential_vel.data = vr;
+			lwheel_tangential_vel.data = vl;
+		}
+
+
+		void spin(){
+
+			ros::Rate rate(20);
+        
+        		// start publishing
+        		while(ros::ok()){
+				rwheel_tang_vel_pub.publish(rwheel_tangential_vel);
+				lwheel_tang_vel_pub.publish(lwheel_tangential_vel);
+				ros::spinOnce();
+				rate.sleep();
+	       		 }
+
+		}
+
+
+
+};
+
+
 int main(int argc, char** argv){
         // Initialize ROS system and create node object
         ros::init(argc, argv,"diffdrive");
         ros::NodeHandle nh;
 
-        // Create and initialize subscriber object
-        ros::Subscriber vel_sub = nh.subscribe("/cmd_vel", 10, cmd_vel_callback);
+        // Create class instance
+	DiffDrive diffdrive = Diffdrive(&nh);
+	
+	//Run the main program
+	diffdrive.spin();
         
-        // Create and initialize publisher objects
-        ros::Publisher  rwheel_tang_vel_pub = nh.advertise<std_msgs::Float32>("/rwheel_tangential_vel", 10); 
-        ros::Publisher  lwheel_tang_vel_pub = nh.advertise<std_msgs::Float32>("/lwheel_tangential_vel", 10); 
-        
-        ros::Rate rate(20);
-        
-        // start publishing
-        while(ros::ok()){
-                rwheel_tang_vel_pub.publish(rwheel_tangential_vel);
-                lwheel_tang_vel_pub.publish(lwheel_tangential_vel);
-                ros::spinOnce();
-                rate.sleep();
-        }
-
         return 0;
 
 }
