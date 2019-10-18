bdb7a814e8e13dfdf20ecb424b17f85ec4d688a3
Salabson <salabson4@yahoo.co.uk>
2019-10-18 08:40:12 +0000

    Refactor:refactor the gopigo interface code

--- src/gopigo_interface/src/gopigo3_interface.py
+++ src/gopigo_interface/src/gopigo3_interface.py
@@ -4,18 +4,35 @@ import rospy
 from std_msgs.msg import Float32
 import gopigo3
 
-rwheel_motorcmd = Float32()
-lwheel_motorcmd = Float32()
+class GopigoInterface:
+	def __init__(self):
+		self.rwheel_motorcmd = Float32()
+		self.lwheel_motorcmd = Float32()
+		# Create gopigo object driver
+		self.GPG = gopigo3.GoPiGo3()
+		# Create subscriber objects
+    		self.rwheel_motorcmd_sub = rospy.Subscriber("/rwheel_motorcmd", Float32, self.rwheel_motorcmd_callback)
+    		self.lwheel_motorcmd_sub = rospy.Subscriber("/lwheel_motorcmd", Float32, self.lwheel_motorcmd_callback)
 
-GPG = gopigo3.GoPiGo3()
+	
+	def rwheel_motorcmd_callback(self, msg):
+    		self.rwheel_motorcmd.data = msg.data
+    		rospy.loginfo("rmotor_cmd: %f", self.rwheel_motorcmd.data)
 
-def rwheel_motorcmd_callback(msg):
-    rwheel_motorcmd.data = msg.data
-    rospy.loginfo("rmotor_cmd: %f", rwheel_motorcmd.data)
-
-def lwheel_motorcmd_callback(msg):
-    lwheel_motorcmd.data = msg.data
-    rospy.loginfo("lmotor_cmd: %f",lwheel_motorcmd.data)
+	
+	def lwheel_motorcmd_callback(self, msg):
+    		self.lwheel_motorcmd.data = msg.data
+    		rospy.loginfo("lmotor_cmd: %f",self.lwheel_motorcmd.data)
+	
+	def spin(self):
+		rate = rospy.Rate(20)
+    		while not rospy.is_shutdown():
+			self.GPG.set_motor_power(self.GPG.MOTOR_RIGHT,self.rwheel_motorcmd.data)
+			self.GPG.set_motor_power(self.GPG.MOTOR_LEFT,self.lwheel_motorcmd.data)
+			rate.sleep()
+    
+    			# call the callbacks
+   			rospy.spin()
 
 
 
@@ -24,16 +41,9 @@ if __name__ == '__main__' :
     # Initialize ros system
     rospy.init_node("gopigo_interface")
     
-    # Create subscriber objects
-    rwheel_motorcmd_sub = rospy.Subscriber("/rwheel_motorcmd", Float32, rwheel_motorcmd_callback)
-    lwheel_motorcmd_sub = rospy.Subscriber("/lwheel_motorcmd", Float32, lwheel_motorcmd_callback)
+    # Create the class instance
+    gopigo_interface = GopigoInterface()
     
-    rate = rospy.Rate(20)
-    while not rospy.is_shutdown():
-	GPG.set_motor_power(GPG.MOTOR_RIGHT,rwheel_motorcmd.data)
-	GPG.set_motor_power(GPG.MOTOR_LEFT,lwheel_motorcmd.data)
-	rate.sleep()
-    
-    # call the callbacks
-    rospy.spin()
-
+    # Run the main program
+    gopigo_interface.spin()   
+   
