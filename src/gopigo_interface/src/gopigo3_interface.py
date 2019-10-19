6c403909d2ef8e8e7fcbfd1d05d461b3cef71f3d
Salabson <salabson4@yahoo.co.uk>
2019-10-19 07:45:10 +0000

    Feat:read encoders

--- src/gopigo_interface/src/gopigo3_interface.py
+++ src/gopigo_interface/src/gopigo3_interface.py
@@ -2,6 +2,7 @@
 
 import rospy
 from std_msgs.msg import Float32
+from gopigo_interface import Encoder
 import gopigo3
 
 class GopigoInterface:
@@ -10,6 +11,8 @@ class GopigoInterface:
 		self.lwheel_motorcmd = Float32()
 		# Create gopigo object driver
 		self.GPG = gopigo3.GoPiGo3()
+		# Create encoder object
+		self encorder_reading = Encoder()
 		# Create subscriber objects
     		self.rwheel_motorcmd_sub = rospy.Subscriber("/rwheel_motorcmd", Float32, self.rwheel_motorcmd_callback)
     		self.lwheel_motorcmd_sub = rospy.Subscriber("/lwheel_motorcmd", Float32, self.lwheel_motorcmd_callback)
@@ -25,10 +28,19 @@ class GopigoInterface:
     		rospy.loginfo("lmotor_cmd: %f",self.lwheel_motorcmd.data)
 	
 	def spin(self):
+		# Reset encoders
+		self.GPG.offset_motor_encoder(self.GPG.MOTOR_LEFT, self.get_motor_encoder(self.GPG.MOTOR.LEFT))
+		self.GPG.offset_motor_encoder(self.GPG.MOTOR_RIGHT, self.get_motor_encoder(self.GPG.MOTOR.RIGHT))
+
 		rate = rospy.Rate(20)
     		while not rospy.is_shutdown():
+			# send power commands to motors
 			self.GPG.set_motor_power(self.GPG.MOTOR_RIGHT,self.rwheel_motorcmd.data)
 			self.GPG.set_motor_power(self.GPG.MOTOR_LEFT,self.lwheel_motorcmd.data)
+
+			# Reader encoders
+			encoder_reading.left = self.GPG.get_motor_encoder(self.GPG.MOTOR_LEFT)
+			encoder_reading.right = self.GPG.get_motor_encoder(self.GPG.MOTOR_RIGHT)
 			rate.sleep()
     
     		# call the callbacks
