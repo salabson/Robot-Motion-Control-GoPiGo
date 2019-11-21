d1e058d08afa13d3f0b296d2af59e98e15120106
Salabson <salabson4@yahoo.co.uk>
2019-11-21 09:17:42 +0000

    Chore:rename variables

--- src/robot_state_updater/src/robot_state_updater.cpp
+++ src/robot_state_updater/src/robot_state_updater.cpp
@@ -3,25 +3,25 @@
 #include<std_msgs/Float32.h>
 #include<math.h>
 
-std_msgs::Float32 left_angular_vel_enc;
-std_msgs::Float32 right_angular_vel_enc;
+std_msgs::Float32 lwheel_angular_vel_enc;
+std_msgs::Float32 rwheel_angular_vel_enc;
 
 
 int N = 720; // ticks per revolution
 double wheel_radius = 0.03;
 double pi = M_PI;
 
-double prev_left_ticks=0;
-double prev_right_ticks=0;
+double prev_left_encoder_ticks=0;
+double prev_right_encoder_ticks=0;
 
-double  delta_left_ticks=0;
-double  delta_right_ticks=0;
+double  delta_left_encoder_ticks=0;
+double  delta_right_encoder_ticks=0;
 
-double distance_left = 0;
-double distance_right = 0;
+double distance_lwheel = 0;
+double distance_rwheel = 0;
 
-double current_left_ticks =0;
-double current_right_ticks =0;
+double current_left_encoder_ticks =0;
+double current_right_encoder_ticks =0;
 
 
 double delta_time=0;
@@ -30,9 +30,9 @@ double delta_time=0;
 
 
 void encoder_callback(const gopigo_interface::Encoder::ConstPtr& enc_ticks){
- current_left_ticks = enc_ticks->left;
- current_right_ticks = enc_ticks->right;
- ROS_INFO("left tick:%f right tick:%f", current_left_ticks, current_right_ticks);
+ current_left_encoder_ticks = enc_ticks->left;
+ current_right_encoder_ticks = enc_ticks->right;
+ ROS_INFO("left tick:%f right tick:%f", current_left_encoder_ticks, current_right_encoder_ticks);
 }
 
 
@@ -46,8 +46,8 @@ ros::NodeHandle nh;
 ros::Subscriber encoder_sub = nh.subscribe("encoder_reading",10, encoder_callback);
 
 // Create publisher objects
-ros::Publisher left_angular_vel_enc_pub = nh.advertise<std_msgs::Float32>("left_angular_vel_enc",10);
-ros::Publisher right_angular_vel_enc_pub = nh.advertise<std_msgs::Float32>("right_angular_vel_enc",10);
+ros::Publisher lwheel_angular_vel_enc_pub = nh.advertise<std_msgs::Float32>("lwheel_angular_vel_enc",10);
+ros::Publisher rwheel_angular_vel_enc_pub = nh.advertise<std_msgs::Float32>("rwheel_angular_vel_enc",10);
 
 ros::Time current_timestamp = ros::Time::now();
 ros::Time last_timestamp = ros::Time::now();
@@ -59,27 +59,30 @@ current_timestamp = ros::Time::now();
 delta_time = (current_timestamp-last_timestamp).toSec();
 
 // calculate diff between two encoder readings
-delta_left_ticks = current_left_ticks - prev_left_ticks;
-delta_right_ticks = current_right_ticks - prev_right_ticks;
+delta_left_encoder_ticks = current_left_encoder_ticks - prev_left_encoder_ticks;
+delta_right_encoder_ticks = current_right_encoder_ticks - prev_right_encoder_ticks;
 
 // calculate distance travel by left and right wheels
-distance_left = delta_left_ticks/N*2*pi*wheel_radius;
-distance_right = delta_right_ticks/N*2*pi*wheel_radius;
+distance_lwheel = delta_left_encoder_ticks/N*2*pi*wheel_radius;
+distance_rwheel = delta_right_encoder_ticks/N*2*pi*wheel_radius;
 
 // calculate encoder tangential velocities for both left and right wheels
-double vl = distance_left/delta_time;
-double vr = distance_right/delta_time;
+double vl = distance_lwheel/delta_time;
+double vr = distance_rwheel/delta_time;
 
 // calculate encoder angular velocities for both left and right wheels
-left_angular_vel_enc.data = vl/wheel_radius;
-right_angular_vel_enc.data = vr/wheel_radius;
+lwheel_angular_vel_enc.data = vl/wheel_radius;
+rwheel_angular_vel_enc.data = vr/wheel_radius;
 
 // publish encoder angular velocities 
-left_angular_vel_enc_pub.publish(left_angular_vel_enc);
-right_angular_vel_enc_pub.publish(right_angular_vel_enc);
+lwheel_angular_vel_enc_pub.publish(lwheel_angular_vel_enc);
+rwheel_angular_vel_enc_pub.publish(rwheel_angular_vel_enc);
 
 // save current data for next iteration
 last_timestamp = current_timestamp;
+prev_left_encoder_ticks = current_left_encoder_ticks;
+prev_right_encoder_ticks = current_right_encoder_ticks;
+
 ros::spinOnce();
 rate.sleep();
 
